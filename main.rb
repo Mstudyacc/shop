148f959b120bdefe32f85e01d91657c2f27b498a
Max <maximzakharov5@gmail.com>
2021-10-12 15:11:59 +0300

    Add UTF-8 strings for russian letters

--- main.rb
+++ main.rb
@@ -1,3 +1,13 @@
+if Gem.win_platform?
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+
 require_relative "product"
 require_relative "book"
 require_relative "film"
